f3e1a318e565e18a6ec52dc9420174d365bc1899
Marcos Souza <marcos@localhost.home>
2015-03-30 20:50:26 -0300

    ...

--- umount_image.sh
+++ umount_image.sh
@@ -2,7 +2,7 @@
 #$1 --> nome da imagem
 
 umount /mnt/$1/sys
-umount /dev/pts /mnt/$1/dev/pts
-umount /dev /mnt/$1/dev
-umount /proc /mnt/$1/proc
+umount /mnt/$1/dev/pts
+umount /mnt/$1/dev
+umount /mnt/$1/proc
 umount /mnt/$1/
