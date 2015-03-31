e2d28dc51190c7ade6fb7f4b4d35067cd8090344
Marcos Souza <marcos@localhost.home>
2015-03-31 02:19:35 -0300

    somes database bugs fixed

--- DataBase.rb
+++ DataBase.rb
@@ -2,46 +2,49 @@ require 'sqlite3'
 
 class DataBase
 	attr_reader :db
-	def initialize(path)
-		@db = SQLite3::Database.open(path)
-		@db.execute("CREATE TABLE IF NOT EXISTS Users(UserId INTEGER PRIMARY KEY, NetworkID INTEGER, Name TEXT)")
-		@db.execute("CREATE TABLE IF NOT EXISTS Templates(TemplateId INTEGER PRIMARY KEY, UserId INTEGER, NebulaId INTEGER, Name TEXT, FOREIGN KEY(UserId) REFERENCES Users(UserId))")
-		@db.execute("CREATE TABLE IF NOT EXISTS VMs(VMId INTEGER PRIMARY KEY, UserId INTEGER, TemplateId INTEGER, NebulaId INTEGER, Name TEXT, FOREIGN KEY(UserId) REFERENCES Users(UserId),FOREIGN KEY(TemplateId) REFERENCES Templates(TemplateId))")
+	def initialize()
 	end
 
-	def insertUser(networkId, name)
+	def DataBase.open(path)
+		@@db = SQLite3::Database.open(path)
+		@@db.execute("CREATE TABLE IF NOT EXISTS Users(UserId INTEGER PRIMARY KEY, NetworkID INTEGER, Name TEXT)")
+		@@db.execute("CREATE TABLE IF NOT EXISTS Templates(TemplateId INTEGER PRIMARY KEY, UserId INTEGER, NebulaId INTEGER, Name TEXT, FOREIGN KEY(UserId) REFERENCES Users(UserId))")
+		@@db.execute("CREATE TABLE IF NOT EXISTS VMs(VMId INTEGER PRIMARY KEY, UserId INTEGER, TemplateId INTEGER, NebulaId INTEGER, Name TEXT, FOREIGN KEY(UserId) REFERENCES Users(UserId),FOREIGN KEY(TemplateId) REFERENCES Templates(TemplateId))")
+	end
+
+	def DataBase.insertUser(networkId, name)
 		string = "INSERT INTO Users VALUES(NULL,"+networkId+",'"+name+"')"
-		@db.execute(string)
+		@@db.execute(string)
 	end
 
-	def removeUser(userId)
+	def DataBase.removeUser(userId)
 		string = "DELETE FROM Users WHERE UserId = "+userId
-		@db.execute(string)
+		@@db.execute(string)
 	end
 
-	def insertTemplate(userId, nebulaId, name)
+	def DataBase.insertTemplate(userId, nebulaId, name)
 		string = "INSERT INTO Templates VALUES(NULL,"+userId+","+nebulaId+",'"+name+"')"
-		@db.execute(string)
+		@@db.execute(string)
 	end
 
-	def removeTemplate(templateId)
+	def DataBase.removeTemplate(templateId)
 		string = "DELETE FROM Templates WHERE TemplateId = "+templateId
-		@db.execute(string)
+		@@db.execute(string)
 	end
 
-	def insertVm(userId, templateId, nebulaId, name)
+	def DataBase.insertVm(userId, templateId, nebulaId, name)
 		string = "INSERT INTO VMs VALUES(NULL,"+userId+","+templateId+","+nebulaId+",'"+name+"')"
-		@db.execute(string)
+		@@db.execute(string)
 	end
 
-	def removeVm(vmId)
+	def DataBase.removeVm(vmId)
 		string = "DELETE FROM VMs WHERE VMId = "+vmid
-		@db.execute(string)
+		@@db.execute(string)
 	end
 
-	def getData(select,from,where = '1')
+	def DataBase.getData(select,from,where = '1')
 		string = "SELECT "+select+" FROM "+from+" WHERE "+where
-		stmt = @db.prepare(string)
+		stmt = @@db.prepare(string)
 		rs = stmt.execute
 		return rs
 	end
