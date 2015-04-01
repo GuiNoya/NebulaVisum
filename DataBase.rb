db26e8c21df197e88c9556966ffe3db2a0ca02cd
GuiNoya <gui.noya@gmail.com>
2015-04-01 03:51:16 -0300

    Added Softwares Table

--- DataBase.rb
+++ DataBase.rb
@@ -12,6 +12,7 @@ class DataBase
 		@@db.execute("CREATE TABLE IF NOT EXISTS Users(UserId INTEGER PRIMARY KEY, NetworkId INTEGER, Name TEXT UNIQUE, ImgCount INTEGER)")
 		@@db.execute("CREATE TABLE IF NOT EXISTS Templates(TemplateId INTEGER PRIMARY KEY, UserId INTEGER, NebulaId INTEGER, Name TEXT UNIQUE, FOREIGN KEY(UserId) REFERENCES Users(UserId))")
 		@@db.execute("CREATE TABLE IF NOT EXISTS VMs(VMId INTEGER PRIMARY KEY, UserId INTEGER, TemplateId INTEGER, NebulaId INTEGER, Name TEXT UNIQUE, FOREIGN KEY(UserId) REFERENCES Users(UserId), FOREIGN KEY(TemplateId) REFERENCES Templates(TemplateId))")
+		@@db.execute("CREATE TABLE IF NOT EXISTS Softwares(SId INTEGER PRIMARY KEY, TemplateId INTEGER, Software TEXT NOT NULL, FOREIGN KEY(TemplateId) REFERENCES Templates(TemplateId))")
 	end
 
 	def DataBase.insertUser(networkId, name)
@@ -41,6 +42,16 @@ class DataBase
 
 	def DataBase.removeVm(vmId)
 		string = "DELETE FROM VMs WHERE VMId = " + vmid
+		@@db.execute(string)
+	end
+
+	def DataBase.insertSoftware(templateId, software)
+		string = "INSERT INTO Softwares VALUES(NULL, " + templateId + "," + software + ")"
+		@@db.execute(string)
+	end
+
+	def DataBase.removeSoftware(templateId, software)
+		string = "DELETE FROM Softwares WHERE TemplateId = " + templateId + " AND Software = '" + software + "')"
 		@@db.execute(string)
 	end
 
